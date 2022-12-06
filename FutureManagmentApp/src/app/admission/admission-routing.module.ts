27aa87d814cd3f37bb6ef6975b82ce8d4ceb14ce
Deepak Yadav <deepakyadavgup61@gmail.com>
2022-12-06 14:41:33 +0530

    I Did My Task

--- FutureManagmentApp/src/app/admission/admission-routing.module.ts
+++ FutureManagmentApp/src/app/admission/admission-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdmissionComponent } from './admission.component';
+import { LeadComponent } from './components/lead/lead.component';
 
-const routes: Routes = [{ path: '', component: AdmissionComponent }];
+const routes: Routes = [{ path: '', component: AdmissionComponent },
+{path:"lead",component:LeadComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- FutureManagmentApp/src/app/app-routing.module.ts
+++ FutureManagmentApp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'admission', loadChildren: () => import('./admission/admission.module').then(m => m.AdmissionModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./admission/admission.module').then(m => m.AdmissionModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
