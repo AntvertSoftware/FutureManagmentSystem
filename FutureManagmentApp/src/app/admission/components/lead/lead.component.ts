c4a6fdf6a7aa413e095dd62900d6e0189ffce6c3
Deepak Yadav <deepakyadavgup61@gmail.com>
2022-12-10 12:40:12 +0530

    solve some errors

--- FutureManagmentApp/src/app/admission/components/lead/lead.component.ts
+++ FutureManagmentApp/src/app/admission/components/lead/lead.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lead',
@@ -6,10 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lead.component.css']
 })
 export class LeadComponent implements OnInit {
+  formRegister!:FormGroup;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.registrationForm();
   }
+
+  registrationForm(){
+   this. formRegister = new FormGroup({
+      firstname: new FormControl("", [Validators.required]),
+      lastname: new FormControl("", [Validators.required]),
+      fathername: new FormControl("", [Validators.required]),
+      mothername: new FormControl(""),
+      gender: new FormControl(""),
+      email: new FormControl(""),
+      mobileno: new FormControl(""),
+      dob: new FormControl(""),
+      class: new FormControl(""),
+      address: new FormControl(""),
+      city: new FormControl(""),
+      state: new FormControl(""),
+      gip: new FormControl(""),
+    });
+  }
+
+  get f()
+  {
+    return this.formRegister.controls;
+  }
+
+  get firstname() { return this.formRegister.get('firstname'); }
+
+    
+  
+ 
+
 
 }
